605ae29081a80f895dee182f23b5cc36fe14d5f3
Benoît Hubert <benoithubert@gmail.com>
2019-08-20 19:08:22 +0200

    Initial commit

---
+++ precache-manifest.37d14909a8225a2e9893076611d1d580.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87844395e08ae0e819f08fd468eec017",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9dd0b05c2c053a49707c",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "dbcb5727326c962b3d8d",
+    "url": "/static/js/2.debab6ac.chunk.js"
+  },
+  {
+    "revision": "9dd0b05c2c053a49707c",
+    "url": "/static/js/main.fad8a59d.chunk.js"
+  },
+  {
+    "revision": "dcbe206d644dc7528ddc",
+    "url": "/static/js/runtime~main.0d748052.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
